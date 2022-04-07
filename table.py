c790061f61ccf1e5826331f8677faa32205c2229
Akshara Mehta <89603333+akshara01@users.noreply.github.com>
2022-04-07 18:17:45 +0530

    Update table.py

--- table.py
+++ table.py
@@ -1,5 +1,5 @@
 import  mysql.connector as sql
-conn=sql.connect(host='localhost',user='root',passwd='#Sonachandi01',database='bank')
+conn=sql.connect(host='localhost',user='root',passwd='(your root password)',database='bank')
 #if conn.is_connected():
      #print('connected succesfully')
 cur = conn.cursor()
