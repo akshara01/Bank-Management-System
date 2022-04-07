52618554dc6f9a841da9ab1125b7899662b07a97
Akshara Mehta <89603333+akshara01@users.noreply.github.com>
2022-04-07 18:14:57 +0530

    Update main bank.py

--- main bank.py
+++ main bank.py
@@ -1,6 +1,6 @@
 
 import  mysql.connector as sql
-conn=sql.connect(host='localhost',user='root',passwd='#Sonachandi01',database='bank')
+conn=sql.connect(host='localhost',user='root',passwd='#(your root password)',database='bank')
 cur = conn.cursor()
 #cur.execute('create table user_table(username varchar(25) primary key,passwrd varchar(25) not null )')
 print('=========================WELCOME TO HDFS BANK============================================================')
